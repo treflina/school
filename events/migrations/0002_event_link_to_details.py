c400d4ee3af2ca15e3ff9008e59b8022e279c1a3
treflina <treflina@wp.pl>
2023-10-23 13:09:12 +0200

    removed dependency in migrations in events

--- events/migrations/0002_event_link_to_details.py
+++ events/migrations/0002_event_link_to_details.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("wagtailcore", "0090_merge_20231002_1822"),
+        # ("wagtailcore", "0090_merge_20231002_1822"),
         ("events", "0001_initial"),
     ]
 
