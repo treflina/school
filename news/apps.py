b29920ada1f2587f7a9702e0f3bc67461f0b2385
treflina <treflina@wp.pl>
2023-11-16 12:08:10 +0100

    limit category choices in news

--- news/apps.py
+++ news/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+from django.db.models import ForeignKey
 
 
 class NewsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "news"
+
+    def ready(self):
+        from wagtail.admin.forms.models import register_form_field_override
+        from core.models import CategorySnippet
+
+        queryset = CategorySnippet.objects.exclude(name="Galeria")
+
+        register_form_field_override(
+            ForeignKey, to=CategorySnippet, override={"queryset": queryset}
+        )
